d8d2caf8c881e37b83679f0a5927a0fe6cd6945e
zahrayasin786 <zahrayasinali123@gmail.com>
2023-02-15 16:32:24 +0500

    change into php

--- wp-config.php
+++ wp-config.php
@@ -21,6 +21,7 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'phase2-wp' );
+ywqegt23i4l
 
 /** Database username */
 define( 'DB_USER', 'root' );
